b9e21c8ec08e0c5659fff566844baba99d620f8b
vira-tresyana <71061869+vira-tresyana@users.noreply.github.com>
2020-12-10 15:38:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/virashop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.virashop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.belajar_constraint_layout)
+    }
+}
